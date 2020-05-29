cdb0b11104d9fd07fef3718514e2629c5783ab3a
Kevin Dean <kevin.dean@utsouthwestern.edu>
2020-05-29 13:56:17 -0500

    20200529
    removing unnecessary functions

--- mesoSPIM/mesoSPIM_Control.py
+++ mesoSPIM/mesoSPIM_Control.py
@@ -1,26 +1,22 @@
+import time
+import logging
+import os
+import sys
+import importlib.util
+from PyQt5 import QtWidgets
+from src.mesoSPIM_MainWindow import mesoSPIM_MainWindow
+
 '''
 mesoSPIM_control.py
 ========================================
 The core module of the mesoSPIM software
 '''
 
-''' Configuring the logging module before doing anything else'''
-import time
-import logging
 timestr = time.strftime("%Y%m%d-%H%M%S")
 logging_filename = timestr + '.log'
 logging.basicConfig(filename='log/'+logging_filename, level=logging.INFO, format='%(asctime)-8s:%(levelname)s:%(threadName)s:%(thread)d:%(module)s:%(name)s:%(message)s')
 logger = logging.getLogger(__name__)
 logger.info('mesoSPIM-control started')
-
-import os
-import sys
-import importlib.util
-
-from PyQt5 import QtWidgets
-
-from src.mesoSPIM_MainWindow import mesoSPIM_MainWindow
-
 logger.info('Modules loaded')
 
 def load_config():
@@ -32,9 +28,6 @@ def load_config():
     cfg_app = QtWidgets.QApplication(sys.argv)
     current_path = os.path.abspath('./config')
     global_config_path = r'C:\Users\ASLM\Desktop\mesoSPIM-utsw\mesoSPIM\config\mesospim_config.py'
-    # global_config_path = ''
-    # global_config_path , _ = QtWidgets.QFileDialog.getOpenFileName(None,\
-    # 'Open microscope configuration file',current_path)
 
     if global_config_path != '':
         ''' Using importlib to load the config file '''
@@ -53,14 +46,6 @@ def load_config():
     sys.exit(cfg_app.exec_())
 
 def stage_referencing_check(cfg):
-    '''
-    Due to problems with some PI stages loosing reference information
-    after restarting the mesoSPIM software, some stage configurations require
-    a reference movement to be carried out before starting the rest of the software.
-
-    As reference movements can damage the instrument, this function warns users
-    about this problem by message boxes and asks them to reach a safe state.
-    '''
     if cfg.stage_parameters['stage_type'] == 'PI_rotzf_and_Galil_xy' or cfg.stage_parameters['stage_type'] == 'PI_rotz_and_Galil_xyf':
         warning = QtWidgets.QMessageBox.warning(None,'Sample z reference movement necessary!',
                 'Please move the XYZ stage to position where a reference z movement (to the midpoint of the movement range) is safe!',
@@ -76,9 +61,6 @@ def stage_referencing_check(cfg):
         return True
 
 def main():
-    """
-    Main function
-    """
     logging.info('mesoSPIM Program started.')
     cfg = load_config()
     app = QtWidgets.QApplication(sys.argv)
